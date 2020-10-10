10b704cd2e48fc5b7e9a525ad380f38ba628bc5f
nels3 <kornelialukojc@gmail.com>
2020-10-10 13:29:52 +0200

    Custom msg package with start msg in it

---
+++ src/control/src/start_signal_publisher.cpp
@@ -0,0 +1,42 @@
+#include <chrono>
+#include <memory>
+#include "rclcpp/rclcpp.hpp"
+#include "control/msg/control.hpp" 
+#include <string>
+
+using namespace std::chrono_literals;
+
+class StartNode : public rclcpp::Node{
+public:
+    StartNode() : Node("StartNode"){
+         this->declare_parameter<std::string>("topic", "control");
+         std::string topic;
+         get_parameter<std::string>("topic", topic);
+         this->declare_parameter<int>("start", 1);
+         get_parameter<int>("start", start_);
+         
+         control_pub = this->create_publisher<control::msg::Control>(topic, 10);
+         
+         timer_ = this->create_wall_timer(500ms, std::bind(&StartNode::timer_callback, this));
+        
+    }
+private:
+    void timer_callback(){
+         auto message = control::msg::Control();                             
+         message.start = start_;                                     
+         control_pub->publish(message);
+    }
+  
+     rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<control::msg::Control>::SharedPtr control_pub;
+    int start_;
+    
+};
+
+int main(int argc, char ** argv){
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<StartNode>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+}
+
